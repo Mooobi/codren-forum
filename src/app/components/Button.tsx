d59caed594d7cb1b17a07497f975ac597a5050a1
Mooobi <antod123@naver.com>
2023-10-12 22:09:18 +0900

    Feat: Button 컴포넌트 구현

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,41 @@
+import { hexToRgb } from '@/util/hexToRgb';
+import styled from 'styled-components';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  background?: string;
+  color?: string;
+}
+
+export default function Button(props: ButtonProps) {
+  return <SC_Button {...props}>{props.children}</SC_Button>;
+}
+
+const SC_Button = styled.button.withConfig({
+  shouldForwardProp: (prop) => prop !== 'background',
+})<ButtonProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  min-width: 4rem;
+  background: ${(props) => props.background};
+  color: ${(props) => props.color};
+  transition: background 0.3s, color 0.3s;
+
+  &:hover {
+    background: ${(props) => props.background && hexToRgb(props.background, 0.8)};
+    color: ${(props) => props.color && hexToRgb(props.color, 0.8)};
+  }
+
+  &:active {
+    background: ${(props) => props.background && hexToRgb(props.background, 0.6)};
+    color: ${(props) => props.color && hexToRgb(props.color, 0.6)};
+  }
+
+  &:disabled {
+    background: #bbbbbb;
+    cursor: default;
+  }
+`;
