65fa726897c1a4fb6a28be3150b27d351bc05a8f
Mooobi <antod123@naver.com>
2023-10-17 18:26:36 +0900

    Feat: 모달창 클릭 시에 이벤트 중단, 스타일 설정

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -33,15 +33,17 @@ export default function useModal() {
     }
     return (
       <Backdrop onClick={closeModal}>
-        <ModalBox>
+        <ModalBox onClick={(e) => e.stopPropagation()}>
           <Message>{message}</Message>
           {children}
-          <Button background={background} color={color} onClick={clickHandler}>
-            {buttonName}
-          </Button>
-          <Button background='#444444' color='white' onClick={closeModal}>
-            취소
-          </Button>
+          <ButtonSection>
+            <Button background={background} color={color} onClick={clickHandler}>
+              {buttonName}
+            </Button>
+            <Button background='#444444' color='white' onClick={closeModal}>
+              취소
+            </Button>
+          </ButtonSection>
         </ModalBox>
       </Backdrop>
     );
@@ -50,8 +52,35 @@ export default function useModal() {
   return { isModalOpen, openModal, closeModal, Modal };
 }
 
-const Backdrop = styled.div``;
+const Backdrop = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
 
-const ModalBox = styled.div``;
+const ModalBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  background: white;
+  padding: 2rem;
+  border-radius: 0.5rem;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+`;
 
 const Message = styled.div``;
+
+const ButtonSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
